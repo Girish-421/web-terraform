b7419738eb8b7d780657154e6309e450be0b41ac
Girish-421 <girishbankupalli@gmail.com>
2024-11-16 13:43:51 +0530

    changes made

--- variable.tf
+++ variable.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "bucket_name" {
   description = "The name of the S3 bucket for static website hosting"
   type        = string
-  default     = "giriw"  # Ensure the name is globally unique
+  default     = "girie"  # Ensure the name is globally unique
 }
 
 variable "index_html_path" {
